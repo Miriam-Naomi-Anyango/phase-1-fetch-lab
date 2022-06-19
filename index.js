ddc39a0642b57d9684b6bb35d8974dede053b319
Miriam Anyango <minaoanya@gmail.com>
2022-06-19 16:01:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+         .then(res => res.json())
+         .then(data => renderBooks(data))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(data) {
+  const main = document.querySelector("main");
+  data.forEach(book => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function() {
+ fetchBooks();
 });
+
+
